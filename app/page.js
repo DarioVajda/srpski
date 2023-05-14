3ddeeaec24a0d1bd3e6eedb79a6f146a8c9cc368
Dario Vajda <vajdadario@gmail.com>
2023-05-14 18:38:12 +0200

    pocet rad na home stranici

--- app/page.js
+++ app/page.js
@@ -1,95 +1,67 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
 
-export default function Home() {
+import React, { useState } from 'react'
+
+import styles from './[lektira]/page.module.css';
+
+import Header from '@/components/skelet/header/Header';
+import Sidebar from '@/components/skelet/sidebar/Sidebar';
+import Footer from '@/components/skelet/footer/Footer';
+
+import Paragraf from '@/components/blokovi/paragraf/Paragraf';
+import Podnaslov from '@/components/blokovi/podnaslov/Podnaslov';
+import YoutubeVideo from '@/components/blokovi/youtubeVideo/YoutubeVideo';
+import Razmak from '@/components/blokovi/razmak/Razmak';
+import Slika from '@/components/blokovi/slika/Slika';
+import Pdf from '@/components/blokovi/pdf/Pdf';
+
+function pickComponent(args){
+  switch (args.blok) {
+    case 'podnaslov':
+      return <Podnaslov tekst={args.tekst}/>
+    case 'paragraf':
+      return <Paragraf tekst={args.tekst}/>
+    case 'ytVideo':
+      return <YoutubeVideo href={args.href}/>
+    case 'slika':
+      return <Slika putanja={args.putanja}/>
+    case 'pdf':
+      return <Pdf putanja={args.putanja}/>
+    case 'razmak':
+      return <Razmak/>
+    default:
+      return <div>{args.blok}</div>
+  }
+}
+
+export default function Page({ }) {
+
+  const home = {
+    
+  }
+
+  const [ showingNavBar, setShowingNavBar ] = useState(false);
+
+  const showNavBar = () => {
+    console.log(!showingNavBar);
+    setShowingNavBar(!showingNavBar);
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.wrapper}>
+      <Header lektira={'home'} showNavBar={showNavBar} />
+      <div>
+        <Sidebar lektira={'home'} showing={showingNavBar} />
+        <div className={styles.main}>
+          {
+            home.map((element, index) => (
+              // <div id={index}>{element.blok}</div>
+              <div key={index}>{pickComponent(element)}</div>
+            ))
+          }
+          <Footer lektira={'home'} />
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
