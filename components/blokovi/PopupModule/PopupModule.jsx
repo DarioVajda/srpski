755f42415254b027ee225583819cc45fdefb1c61
Dario Vajda <vajdadario@gmail.com>
2023-05-14 17:21:53 +0200

    dodao sam popup module

---
+++ components/blokovi/PopupModule/PopupModule.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import ReactDom from 'react-dom'
+import useScrollbarSize from 'react-scrollbar-size';
+
+
+const PopupModule = ({ children, open, height, width, unit }) => {
+  
+  const [cash, setCash] = useState(false);
+
+  const scrollBar = useScrollbarSize();
+  // console.log({scrollBar}, window.visualViewport.width < window.Width)
+
+  const wrapperStyle = {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.384)'
+  }
+
+  const parentStyle = {
+    backgroundColor: 'var(--background)',
+    border: '1px solid var(--light-text)',
+    borderRadius: '20px',
+    // overflow: 'hidden',
+    padding: '0 1em',
+    width: `${width?width:60}${unit?unit:'%'}`,
+    height: `${height?height:70}${unit?unit:'%'}`,
+    display: 'flex',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
+    flexDirection: 'column'
+  }
+
+  if(typeof document === 'undefined') {
+    return <></>;
+  }
+
+  if(!open) {
+    document.body.style.overflow = 'visible';
+    document.body.style.marginRight = '0';
+    return <></>;
+  }
+  
+  document.body.style.overflow = 'hidden';
+  document.body.style.marginRight = window.visualViewport.width < window.Width ? `${scrollBar.width}px` : '0px';
+
+  return ReactDom.createPortal(
+    <div style={wrapperStyle} >
+      <div style={parentStyle} >
+        { React.cloneElement(children, { saveData: setCash, data: cash }) }
+      </div>
+    </div>,
+    document.getElementById('popup')
+  )
+}
+
+export default PopupModule
