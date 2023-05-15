2f765a9bb9c97aafbabade1c1f01cc372ee47909
OgnjenTesic <89947464+OgnjenTesic@users.noreply.github.com>
2023-05-15 23:06:43 +0200

    Update Footer.jsx

--- components/skelet/footer/Footer.jsx
+++ components/skelet/footer/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = ({ lektira }) => {
   return (
     <div className={styles.footer}>
       <span>prof. Aleksandar Slavov</span>
-      <span>kontakt tel: +38163333333</span>
-      <span>email: mejl.adresa@gmail.com</span>
+      <span>kontakt tel: +381642389987</span>
+      <span>email: slovoslavovs@gmail.com</span>
       <span>
-        Stranicu napravila i dizajnirala tri učenika 4. it (2019-2023), najboljeg odeljenja u istoriji subotičke gimnazije -{' '}
+        Stranicu napravila i dizajnirala tri učenika 4. it (2019-2023), najboljeg odeljenja u istoriji subotičke gimnazije (po našem skromnom mišljenju :)) -{' '}
         <a target="_blank" href="https://instagram.com/dario_vajda">Dario Vajda</a>, <a target="_blank" href="https://instagram.com/coa_204">Aleksandar Rašković</a> i <a target="_blank" href="https://instagram.com/ognjentesic_">Ognjen Tešić</a>.
       </span>
     </div>
