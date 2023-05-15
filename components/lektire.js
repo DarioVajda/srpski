0d7297646d74733d05f687f0000f727ee8b1ac28
OgnjenTesic <89947464+OgnjenTesic@users.noreply.github.com>
2023-05-15 23:42:27 +0200

    Update lektire.js

--- components/lektire.js
+++ components/lektire.js
@@ -6,17 +6,17 @@ const lektire = {
         sadrzaj: [
             { blok: 'podnaslov', tekst: 'Biografija Pisca (npr)' },
             { blok: 'paragraf', tekst: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodo consequat. Duis aute irure dolor in longinquitate levis, in gravitate brevis soleat esse, ut eius magnitudinem celeritas, diuturnitatem.' },
-            { blok: 'slika', putanja: 'https://play-lh.googleusercontent.com/5B6Z4GWPuAlrnZDtqI8tdlnngu4dg-QG3lD2OTIvyvzSGCRJibEDaAbvQr6TMCPjqUc'},
+            { blok: 'slika', putanja: 'https://daily.jstor.org/wp-content/uploads/2019/06/franz_kafkas_the_trial_its_funny_because_its_true_1050x700.jpg'},
             { blok: 'podnaslov', tekst: 'Analiza dela' },
             { blok: 'paragraf', tekst: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore disputandum putant. Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium.' },
-            { blok: 'link', href: 'http://www.themostamazingwebsiteontheinternet.com/', tekst:'amogus'},
+            { blok: 'link', href: 'http://www.themostamazingwebsiteontheinternet.com/', tekst:'Evo linka'},
             { blok: 'pdf', putanja: 'https://www.rastko.rs/drama/ssd/ssd19/m_milenkovic.pdf'},
             { blok: 'razmak'},
             { blok: 'podnaslov', tekst: 'Likovi' },
             { blok: 'paragraf', tekst: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore disputandum putant. Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium.' },
             { blok: 'ytVideo', href: 'https://www.youtube.com/embed/dQw4w9WgXcQ'},
             { blok: 'slika', putanja: 'sl3.jpg'},
-            { blok: 'slika', putanja: 'sl2.png'},
+            { blok: 'slika', putanja: 'sl2.jpg'},
             { blok: 'pdf', putanja: 'bitcoin.pdf'}
         ]
     },
